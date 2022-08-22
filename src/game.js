9ff2772122391805252c840fc78471a3bd236a5f
Tommosho <tc_lex@hotmail.com>
2022-08-22 03:45:49 -0500

    renderizado del mapa por lvl, objeto literal

--- src/game.js
+++ src/game.js
@@ -3,37 +3,73 @@ const game = canvas.getContext("2d");
 let canvasSize;
 let canvasElement;
 
-//Ahora vamos a sustituir la funcion que se esta llamando, por la de setCanvasSize(), pues asi siempre se va a cargar el juego o el tamaño dependiendo del tamaño de la pagina, y tambien premos llmar otras funciones que se ejecuten, en este caso sera en   mismo juego 
-window.addEventListener('load', setCanvasSize);
+//cree una clase para los mapas, con una propiedad que indica el lvl y una funcion que lo renderiza
+class Map {
+    constructor(){
+        this.lvl=0
+    }
+    render() {
+        if(this.lvl>=maps.length){
+            return console.log ("Ese mapa no existe")
+        }
+        const map =maps[this.lvl].match(/[IXO\-]+/g)
+            .map(a => a.split(""))
+        game.font = canvasElement + "px Verdana"
+        game.textAlign = "end"
+        map.forEach((x, xi) => {
 
+        x.forEach((y, yi) => {
+            const posX = canvasElement * (xi + 1)
+            const posY = canvasElement * (yi + 1)
+            game.fillText(emojis[y], posY, posX)
+        })
+    })
+    }
+}
+const map = new Map()
+
+// const map={
+//     lvl:0,
+//     render:function () {
+//         if(this.lvl>=maps.length){
+//             return console.log ("Ese mapa no existe")
+//         }
+//         console.log(this.lvl)
+//         const Map = maps[this.lvl].match(/[IXO\-]+/g)
+//             .map(a => a.split(""))
+           
+//         game.font = canvasElement + "px Verdana"
+//         game.textAlign = "end"
+//         Map.forEach((x, xi) => {
+
+//         x.forEach((y, yi) => {
+//             const posX = canvasElement * (xi + 1)
+//             const posY = canvasElement * (yi + 1)
+//             game.fillText(emojis[y], posY, posX)
+//         })
+//     })
+//     }
+// }
+// console.log(map())
+window.addEventListener('load', setCanvasSize);
 window.addEventListener('resize', setCanvasSize)
+
 
 function setCanvasSize() {
 
-    if (window.innerHeight > window.innerWidth) {
-        canvasSize = window.innerWidth * 0.9;
+    window.innerHeight > window.innerWidth
+        ? canvasSize = window.innerWidth * 0.9
+        : canvasSize = window.innerHeight * 0.7;
 
-    } else {
-        canvasSize = window.innerHeight * 0.7;
 
-    }
     canvas.setAttribute("height", canvasSize)
     canvas.setAttribute("width", canvasSize)
     canvasElement = canvasSize / 10;
     startGame()
 }
-function startGame() {
-    const map = maps[0].match(/[IXO\-]+/g)
-    .map(a=>a.split(""))
-    console.log(map)
-    game.font = canvasElement + "px Verdana"
-    game.textAlign = "end"
-    for (let x = 1; x <= 10; x++) {
-        for (let y = 1; y <= 10; y++) {
-            game.fillText(emojis[map[x-1][y-1]], canvasElement*y, canvasElement*x)
-        }
-    }
 
+function startGame() {
+    map.lvl=0
+    map.render()
 
 }
-// Cree una funcion donde simplemente estamos empaquetando los tamaños de nuestro canvas 
