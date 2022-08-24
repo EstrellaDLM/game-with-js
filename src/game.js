7b4f2229ab83f902a9f0099204bbf9e3b63454fa
Tommosho <tc_lex@hotmail.com>
2022-08-24 10:55:46 -0500

    Corrección de bugs, agregado boton de Juego nuevo

--- src/game.js
+++ src/game.js
@@ -3,15 +3,28 @@ const game = canvas.getContext("2d");
 const btnUp = document.querySelector('#up');
 const btnLeft = document.querySelector('#left');
 const btnRight = document.querySelector('#right');
-const btnDown = document.querySelector('#down')
-const playerLives =document.getElementById('vidas')
+const btnDown = document.querySelector('#down');
+const playerLives = document.getElementById('vidas');
+const playerTime = document.getElementById('time');
+const record = document.getElementById('record');
+const result = document.getElementById('result');
+const NewGame= document.getElementById("newGame")
 let canvasSize;
 let canvasElement;
-// let lives=3;
-const playerDetails={
-    lives:3,
-    render: function(){
-        livesPlayes()
+let timeInterval
+let time_playing
+const playerDetails = {
+    lives: 3,
+    timeStart:  undefined,
+    timePlaying: undefined,
+    render: function () {
+        showLives()
+
+        if (!this.timeStart) {
+            this.timeStart = Date.now()
+            timeInterval = setInterval(showTime, 100)
+        }
+        showRecord()
     }
 }
 const map = {
@@ -25,12 +38,12 @@ const map = {
             const coincideX = Math.floor(this.x) === Math.floor(map.giftPosition.x);
             const coincideY = Math.floor(this.y) === Math.floor(map.giftPosition.y);
 
-            
+
             if (coincideY && coincideX) {
                 console.log("Subiste de nivel")
                 lvlUp()
             }
-            
+
             resetGame()
         }
 
@@ -42,13 +55,14 @@ const map = {
     bombPosition: [],
     render: function () {
 
-        if(!maps[this.lvl]){
+        if (!maps[this.lvl]) {
             winGame()
             return
         }
+
         const Map = maps[this.lvl].match(/[IXO\-]+/g)
             .map(a => a.split("")) || console.log("No more maps")
-        
+
         game.font = canvasElement + "px Verdana"
         game.textAlign = "end"
         game.clearRect(0, 0, canvasSize, canvasSize)
@@ -83,17 +97,29 @@ const map = {
 
 window.addEventListener('load', setCanvasSize);
 window.addEventListener('resize', setCanvasSize)
+NewGame.addEventListener("click",newGame)
 
+function newGame(){
+    map.lvl=0;
+    playerDetails.lives=3
+    clearInterval( timeInterval )
+    playerDetails.timeStart = undefined
+    map.playerPosition.x = undefined
+    map.playerPosition.y = undefined
+    startGame()
 
+}
 function setCanvasSize() {
     window.innerHeight > window.innerWidth
-        ? canvasSize = window.innerWidth * 0.9
+        ? canvasSize = window.innerWidth * 0.7
         : canvasSize = window.innerHeight * 0.7;
 
 
     canvas.setAttribute("height", canvasSize)
     canvas.setAttribute("width", canvasSize)
     canvasElement = canvasSize / 10;
+    map.playerPosition.x=undefined
+    map.playerPosition.y=undefined
     startGame()
 }
 
@@ -102,7 +128,7 @@ function startGame() {
     map.bombPosition = []
     map.render()
     playerDetails.render()
-
+    
 }
 function lvlUp() {
     map.lvl += 1;
@@ -124,12 +150,14 @@ function colision() {
 
 }
 function resetGame() {
-   
+
     if (colision()) {
         playerDetails.lives--;
-        if(playerDetails.lives<=0){
-            map.lvl=0
-            playerDetails.lives=3
+        if (playerDetails.lives <= 0) {
+            map.lvl = 0
+            playerDetails.lives = 3
+            clearInterval(timeInterval)
+            playerDetails.timeStart = undefined
         }
         map.playerPosition.x = undefined
         map.playerPosition.y = undefined
@@ -137,15 +165,40 @@ function resetGame() {
     }
 
 }
-function winGame(){
+function winGame() {
+    const record =localStorage.getItem("record")
+    if (record===null) {
+        console.log("lo lograste")
+        result.innerText ="Lo LOGRASTE!!!"
+        localStorage.setItem("record", time_playing);
+    }
+    if (time_playing < localStorage.getItem("record")) {
+        localStorage.setItem("record", time_playing);
+        result.innerText = "NUEVO RECORD!"
+  
+    }
+    if (time_playing > localStorage.getItem("record")) {
+        result.innerText = "Sigue intentandolo noob!"
+  
+    }
+    clearInterval(timeInterval)
     console.log("Ganaste")
 }
+
+
 //Detalles del jugador
-function livesPlayes(){
-   const lives =Array(playerDetails.lives).fill(emojis["heart"])
-   playerLives.innerText=""
-   lives.forEach(a=>playerLives.append(a))
+function showLives() {
+    const lives = Array(playerDetails.lives).fill(emojis["heart"]).join("")
+    playerLives.innerText = lives
 }
+function showTime() {
+    time_playing=Date.now() - playerDetails.timeStart
+    playerTime.innerHTML = time_playing
+}
+function showRecord() {
+    record.innerText = localStorage.getItem("record")
+}
+
 
 //moviemientos
 window.addEventListener("keyup", moveByKey)
